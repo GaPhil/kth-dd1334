1827d0431fa7cfd9e279fdb59b43a2c58a55867e
GaPhil <gaphil.io@gmail.com>
2017-09-06 16:15:25 +0200

    Add Task 1.1

---
+++ lab1/queries.sql
@@ -0,0 +1,12 @@
+-- use booktown db instead of personal one to avoid accidentaly modifying db
+\c booktown
+--Task 1
+--1.
+SELECT VALUE last_name, first_name
+FROM authors
+WHERE author_id = (
+	SELECT author_id
+	FROM books
+	WHERE title='The Shining'
+);
+--2.
