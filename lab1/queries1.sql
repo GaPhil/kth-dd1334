e317f2f73ccfa7e1f9418ddddc37ef00a6cde7f0
GaPhil <gaphil.io@gmail.com>
2017-09-09 16:24:44 +0200

    Clean up and rename files

--- lab1/queries1.sql
+++ lab1/queries1.sql
@@ -76,20 +76,20 @@ HAVING COUNT (customer_id) = 2;
 \echo *** Task 1.7 *** \echo
 
 --> TODO
-SELECT name, cost
-SELECT *
-FROM publishers
-INNER JOIN editions
-	ON publishers.publisher_id = editions.publisher_id
-INNER JOIN stock
-	ON editions.isbn = stock.isbn;
+--SELECT name, cost
+--SELECT *
+--FROM publishers
+--INNER JOIN editions
+--	ON publishers.publisher_id = editions.publisher_id
+--INNER JOIN stock
+--	ON editions.isbn = stock.isbn;
 
-SELECT *
-FROM publishers
-INNER JOIN editions
-	ON publishers.publisher_id = editions.publisher_id
-INNER JOIN shipments
-	ON editions.isbn = shipments.isbn;
+--SELECT *
+--FROM publishers
+--INNER JOIN editions
+--	ON publishers.publisher_id = editions.publisher_id
+--INNER JOIN shipments
+--	ON editions.isbn = shipments.isbn;
 --> TODO
 
 \echo *** Task 1.8 *** \echo
