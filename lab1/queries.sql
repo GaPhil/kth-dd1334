3a23e01a3e9793486c51491e2848e0b202f0b000
GaPhil <gaphil.io@gmail.com>
2017-09-09 15:08:33 +0200

    Add Task 1.9

--- lab1/queries.sql
+++ lab1/queries.sql
@@ -93,8 +93,21 @@ INNER JOIN shipments
 --> TODO
 
 \echo *** Task 1.8 *** \echo
+-- total money earnt is profit made on each shipment
 
-SELECT SUM (retail_price - cost)
-FROM shipments
-JOIN stock
-	ON shipments.isbn = stock.isbn;
+SELECT SUM(retail_price - cost)
+FROM shipments, stock
+WHERE shipments.isbn = stock.isbn;
+
+
+\echo *** Task 1.9 *** \echo
+
+SELECT last_name, first_name
+FROM customers, shipments, editions, books, subjects
+WHERE customers.customer_id = shipments.customer_id
+	AND shipments.isbn = editions.isbn
+	AND editions.book_id = books.book_id
+	AND books.subject_id = subjects.subject_id
+GROUP BY customers.last_name, customers.first_name
+HAVING COUNT (subjects.subject_id) > 2;
+
