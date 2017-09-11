4a2bb4c613512aa15c282ebbb7ab672a0973a105
GaPhil <gaphil.io@gmail.com>
2017-09-11 16:32:01 +0200

    WIP triggers.sql

--- lab1/triggers.sql
+++ lab1/triggers.sql
@@ -1,17 +1,32 @@
+\echo *** Task 3 *** \echo
+---> NEW TASK <---
+
+DROP FUNCTION decstock() CASCADE;
+
+CREATE FUNCTION decstock()
+  RETURNS TRIGGER AS $pname$
+BEGIN
+  IF stock.stock == 0
+    THEN RAISE EXCEPTION 'There is no stock to ship';
+  ELSE -- do insert and update stock
+    -- selects most recently added shipment and reduces that isbn's stock
+    UPDATE stock
+    SET stock = stock - 1
+    WHERE stock.isbn = (
+      SELECT isbn
+      FROM shipments
+      ORDER BY ship_date DESC
+      LIMIT 1
+    );
+  END IF;
+  RETURN NEW;
+END;
+
+$pname$ LANGUAGE plpgsql;
+
 CREATE TRIGGER reduce_stock
-ON shipment
-AFTER INSERT
+AFTER INSERT ON shipment
+FOR EACH ROW
+EXECUTE PROCEDURE decstock();
 
--- selects most recently added shipment and reduces that isbn's stock
-
-UPDATE stock
-SET stock = stock - 1
-WHERE stock.isbn = (
-	SELECT isbn
-	FROM shipments
-	ORDER BY ship_date DESC
-	LIMIT 1
-	);
-
-SELECT COUNT(stock)
-FROM stock;
+DROP FUNCTION decstock() CASCADE;
