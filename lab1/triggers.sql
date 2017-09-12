5149003b573411b9d72e5f0b10bc7d47fbe20816
GaPhil <gaphil.io@gmail.com>
2017-09-12 23:33:03 +0200

    Add trigger

--- lab1/triggers.sql
+++ lab1/triggers.sql
@@ -6,20 +6,15 @@ DROP FUNCTION decstock() CASCADE;
 CREATE FUNCTION decstock()
   RETURNS TRIGGER AS $pname$
 BEGIN
-  IF (SELECT NEW.isbn
+  IF (SELECT stock
       FROM stock
-     ) IS NULL
-    THEN RAISE EXCEPTION 'There is no stock to ship';
-  ELSE -- do insert and update stock
-    -- selects most recently added shipment and reduces that isbn's stock
+      WHERE stock.isbn = NEW.isbn) <= 0
+  THEN
+    RAISE EXCEPTION 'There is no stock to ship!';
+  ELSE
     UPDATE stock
     SET stock = stock - 1
-    WHERE stock.isbn = (
-      SELECT isbn
-      FROM shipments
-      ORDER BY ship_date DESC
-      LIMIT 1
-    );
+    WHERE stock.isbn = new.isbn;
   END IF;
   RETURN NEW;
 END;
@@ -31,12 +26,31 @@ AFTER INSERT ON shipments
 FOR EACH ROW
 EXECUTE PROCEDURE decstock();
 
--- Output: the whole table before making shipments.
+\echo *** PRINT OUT CONTENT OF STOCK *** \echo
+
 SELECT *
 FROM stock;
 
+\echo *** ATTEMPT TO SHIP BOOK THAT IS NOT IN STOCK *** \echo
+
 INSERT INTO shipments
-VALUES (2000, 860, '0394900014', '2012-12-07');
+VALUES(2000, 860, '0394900014', '2012-12-07');
 
--- DROP FUNCTION decstock() CASCADE;
+\echo \echo *** SHIP BOOK THAT IS IN STOCK *** \echo
 
+INSERT INTO shipments
+VALUES(2001, 860, '044100590X', '2012-12-07');
+
+\echo \echo *** SHOW THAT ON SECOND SHIPMENT WAS INSERTED *** \echo
+
+SELECT * FROM shipments WHERE shipment_id > 1999;
+
+\echo \echo *** PRINT OUT CONTENT OF STOCK *** \echo
+
+SELECT * FROM stock;
+
+\echo \echo *** RESTORE DATABASE TO ITS ORIGINAL STATE *** \echo
+DELETE FROM shipments WHERE shipment_id > 1999;
+UPDATE stock SET stock = 89 WHERE isbn = '044100590X';
+
+DROP FUNCTION decstock() CASCADE;
